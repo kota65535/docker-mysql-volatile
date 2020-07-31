0859a10b7776681b8a1ffb0bdc3cb79f83fbb49e
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2020-07-31 19:50:07 +0900

    first commit

---
+++ docker_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+
+FROM="mysql:latest"
+REPOSITORY_NAME="kota65535/mysql-volatile"
+
+docker pull ${FROM}
+curl https://raw.githubusercontent.com/gdraheim/docker-copyedit/master/docker-copyedit.py | python - FROM ${FROM} REMOVE ALL VOLUMES INTO ${REPOSITORY_NAME}
+docker push ${REPOSITORY_NAME}
