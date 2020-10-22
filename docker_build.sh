3200f9aa716b190ce381edc43b029eb4f510cc28
Tomohiko Ozawa <kota65535@gmail.com>
2020-10-23 01:40:54 +0900

    build multiple versions on push

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 set -eux
 
-FROM="mysql:latest"
+usage() {
+  cat << EOF
+usage: bash $(basename $0) --version <version>
+EOF
+}
+
+# parse options
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+      --version)
+      VERSION="$2"
+      shift 2
+      ;;
+      *)    # unknown option
+      shift # past argument
+      ;;
+  esac
+done
+
+FROM="mysql:${VERSION}"
 REPOSITORY_NAME="kota65535/mysql-volatile"
 
 docker pull ${FROM}
-curl https://raw.githubusercontent.com/gdraheim/docker-copyedit/master/docker-copyedit.py | python - FROM ${FROM} REMOVE ALL VOLUMES INTO ${REPOSITORY_NAME}
-docker push ${REPOSITORY_NAME}
+curl https://raw.githubusercontent.com/gdraheim/docker-copyedit/master/docker-copyedit.py | python - FROM ${FROM} REMOVE ALL VOLUMES INTO ${REPOSITORY_NAME}:${VERSION}
+docker push ${REPOSITORY_NAME}:${VERSION}
